467c7b838aee1a5c2a236e5202ede4b0df7ab6a0
sheetal-10 <sheetalp257@gmail.com>
2021-09-21 00:05:36 +0530

    Added: test case for 0 feet and 0 feet should return true

--- src/main/java/com/quantitymeasurement/UnitMeasurement.java
+++ src/main/java/com/quantitymeasurement/UnitMeasurement.java
@@ -1,5 +1,10 @@
 package com.quantitymeasurement;
 
+/**
+ * Main class for printing welcome message
+ * @author sheetal
+ * @since 2021-09-20
+ */
 public class UnitMeasurement {
     public static void main(String[] args) {
         System.out.println("Welcome to Quantity Measurement Problem");
---
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -0,0 +1,15 @@
+package com.quantitymeasurement;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QuantityTest {
+
+    //testcase for 0 feet and 0 feet should return true
+    @Test
+    public void given0Feetand0Feet_ShouldReturnEqual() {
+        Feet feet1 = new Feet (0.0);
+        Feet feet2 = new Feet (0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
+}
