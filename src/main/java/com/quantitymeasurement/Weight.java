a8ae4e17442b7ecc6118fd3ec77c47f954c2cf4e
sheetal-10 <sheetalp257@gmail.com>
2021-09-23 23:13:53 +0530

    Added: code and test case for comparing different weight units

--- src/main/java/com/quantitymeasurement/Weight.java
+++ src/main/java/com/quantitymeasurement/Weight.java
@@ -1,7 +1,10 @@
 package com.quantitymeasurement;
 
 public class Weight {
-    enum Unit {GRAM};
+    enum Unit {GRAM, KILOGRAM, TONNE};
+
+    private static final double KILOGRAM_TO_GRAM = 1000;
+    private static final double TONNE_TO_KILOGRAM = 1000;
 
     private final Unit unit;
     private final double value;
@@ -9,6 +12,18 @@ public class Weight {
     public Weight(Unit unit, double value) {
         this.unit = unit;
         this.value = value;
+    }
+
+    public boolean compare(Weight that) {
+        if (this.unit.equals(Unit.KILOGRAM) && that.unit.equals(Unit.GRAM))
+            return Double.compare(this.value * KILOGRAM_TO_GRAM, that.value) == 0;
+        if (this.unit.equals(Unit.GRAM) && that.unit.equals(Unit.KILOGRAM))
+            return Double.compare(this.value , that.value* KILOGRAM_TO_GRAM) == 0;
+        if (this.unit.equals(Unit.TONNE) && that.unit.equals(Unit.KILOGRAM))
+            return Double.compare(this.value * TONNE_TO_KILOGRAM, that.value) == 0;
+        if (this.unit.equals(Unit.KILOGRAM) && that.unit.equals(Unit.TONNE))
+            return Double.compare(this.value, that.value* TONNE_TO_KILOGRAM) == 0;
+        return false;
     }
 
     @Override
--- src/test/java/com/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -457,4 +457,36 @@ public class QuantityTest {
         Weight gram2 = new Weight(Weight.Unit.GRAM, 1.0);
         Assertions.assertNotEquals(gram1, gram2);
     }
+
+    @Test
+    public void given1KilogramAnd1000Gram_WhenCompared_ShouldReturnEqualWeight() {
+        Weight kilogram1 = new Weight(Weight.Unit.KILOGRAM, 1.0);
+        Weight gram1 = new Weight(Weight.Unit.GRAM, 1000.0);
+        boolean compareCheck = kilogram1.compare(gram1);
+        Assertions.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void given1000GramAnd1Kilogram_WhenCompared_ShouldReturnEqualWeight(){
+        Weight gram1 = new Weight(Weight.Unit.GRAM, 1000.0);
+        Weight kilogram1 = new Weight(Weight.Unit.KILOGRAM, 1.0);
+        boolean compareCheck = gram1.compare(kilogram1);
+        Assertions.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void given1TonneAnd1000Kilogram_WhenCompared_ShouldReturnEqualWeight() {
+        Weight tonne1 = new Weight(Weight.Unit.TONNE, 1.0);
+        Weight kilogram1 = new Weight(Weight.Unit.KILOGRAM, 1000.0);
+        boolean compareCheck = tonne1.compare(kilogram1);
+        Assertions.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void given1000KilogramAnd1Tonne_WhenCompared_ShouldReturnEqualWeight() {
+        Weight kilogram1 = new Weight(Weight.Unit.KILOGRAM, 1000.0);
+        Weight tonne1 = new Weight(Weight.Unit.TONNE, 1.0);
+        boolean compareCheck = tonne1.compare(kilogram1);
+        Assertions.assertTrue(compareCheck);
+    }
 }
